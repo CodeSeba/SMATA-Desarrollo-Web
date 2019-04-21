2348abb03d9e31ed59f548c077271b1affb6ebc4
CodeSeba <seba84x@gmail.com>
2019-04-20 23:18:33 -0300

    Limpieza.

--- Aplicaciones Web/AplicacionesWebClase06/src/main/java/ar/org/centro8/curso/java/web/aplicaciones/utils/ClientHttp.java
+++ Aplicaciones Web/AplicacionesWebClase06/src/main/java/ar/org/centro8/curso/java/web/aplicaciones/utils/ClientHttp.java
@@ -2,7 +2,6 @@ package ar.org.centro8.curso.java.web.aplicaciones.utils;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.Socket;
 import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
