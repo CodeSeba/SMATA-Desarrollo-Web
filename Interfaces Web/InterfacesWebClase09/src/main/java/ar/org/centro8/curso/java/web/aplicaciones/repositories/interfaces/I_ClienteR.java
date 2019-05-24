8402f748b059c59f1106f49a8b1d0ebf967133ec
CodeSeba <seba84x@gmail.com>
2019-05-24 15:24:54 -0300

    Agregado AplicacionesWebClase10. Profe.

---
+++ Interfaces Web/InterfacesWebClase09/src/main/java/ar/org/centro8/curso/java/web/aplicaciones/repositories/interfaces/I_ClienteR.java
@@ -0,0 +1,16 @@
+package ar.org.centro8.curso.java.web.aplicaciones.repositories.interfaces;
+import ar.org.centro8.curso.java.web.aplicaciones.entities.Cliente;
+import ar.org.centro8.curso.java.web.aplicaciones.entities.Factura;
+import ar.org.centro8.curso.java.web.aplicaciones.enums.TipoDocumento;
+import java.util.List;
+public interface I_ClienteR extends I_GenericR<Cliente> {
+    Cliente getById(int id);
+    List<Cliente> getByApellido(String apellido);
+    List<Cliente> getLikeApellido(String apellido);
+    List<Cliente> getByApellidoNombre(String apellido, String nombre);
+    List<Cliente> getByTipoNumero(TipoDocumento tipo, String numero);
+    List<Cliente> getByDireccion(String direccion);
+    List<Cliente> getLikeDireccion(String direccion);
+    Cliente getByFactura(Factura factura);
+    Cliente getByFactura(char letra,int numero);
+}
