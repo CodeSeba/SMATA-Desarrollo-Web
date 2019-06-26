4e139a0a07825d9cde87df1374326498d25ca36d
CodeSeba <seba84x@gmail.com>
2019-06-26 15:50:53 -0300

    Agregado AplicacionesWebClase13 profe, me equivoque con Interfaces.

---
+++ Aplicaciones Web/AplicacionesWebClase13/src/main/java/ar/org/centro8/curso/java/web/aplicaciones/repositories/jpa/ArticuloR.java
@@ -0,0 +1,41 @@
+package ar.org.centro8.curso.java.web.aplicaciones.repositories.jpa;
+import ar.org.centro8.curso.java.web.aplicaciones.entities.Articulo;
+import ar.org.centro8.curso.java.web.aplicaciones.repositories.interfaces.I_ArticuloR;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+public class ArticuloR implements I_ArticuloR{
+    private EntityManagerFactory emf;
+    public ArticuloR(EntityManagerFactory emf) { this.emf = emf; }
+    @Override public void save(Articulo e) {
+        if(e==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+    @Override public void remove(Articulo e) {
+        if(e==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+    @Override public void update(Articulo e) {
+        if(e==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+    @Override public List<Articulo> getAll() {
+        return emf
+                .createEntityManager()
+                .createNamedQuery("Articulo.findAll")
+                .getResultList();
+    }
+    
+}
